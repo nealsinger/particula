d3448d5fb1ddec7796da2f5f875a407003d5a16f
NealS <poptire@gmail.com>
2015-07-27 23:20:23 -0500

    add distance measuring between bot and landmark

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+
+/*
+
+//Some Rust references:
+
+	let x: &[(usize, usize)] = &[(1,2), (3,9)];
+	for &(a,b) in x.iter() {
+		println!("a is {}, b is {}", a, b);
+	}
+	
+*/
+
+
 extern crate rand;
 use rand::Rng;
 use rand::distributions::normal::StandardNormal;
@@ -15,24 +28,22 @@ impl Robot {
         self.y += d_y;
         self.heading += d_heading;
     }
-    /*
-    fn sense(&mut self, landmarks) {
-    
-    	for i in landmarks.len() {
-    		dist = sqrt( (self.x - ) )
-    	
-    	} 
-    
-    
-    }*/
+	
+		
+	
+    fn subtract_distances( &mut self, landmarks: &[(f32, f32)] ) {
+		for (i, &(x, y)) in landmarks.iter().enumerate() {    	
+			println!("distance between robot and landmark {} is ({}, {}),", i, self.x - x, self.y - y);		
+		}
+    }
 }
 
+
+
+
+
 fn main() {
-	let x: &[(usize, usize)] = &[(1,2), (3,9)];
-	for &(a,b) in x.iter() {
-		println!("a is {}, b is {}", a, b);
-	}
-//	let vector: Vec<i32> = vec![(0,0), (10, 0)];
+
 	
 	
 	let StandardNormal(x) = rand::random();
@@ -45,12 +56,14 @@ fn main() {
     println!("My bot is at ({}, {}), with heading {} radians",
                my_bot.x, my_bot.y, my_bot.heading);
 
+	let x: &[(f32, f32)] = &[(1.0,2.0), (3.0,9.0)];
+	my_bot.subtract_distances(x);
     let mut v: Vec<Robot> = Vec::new();
     v.push(my_bot);
     println!("length of vector is {}", v.len());
     
 
-    
+ /*   
     for i in 0 .. 10 {
 		let mut temp_x: f32 = rng.gen_range(-10.0f32, 4.0e1f32);
 		let mut temp_y: f32 = rng.gen_range(-10.0f32, 4.0e1f32);
@@ -58,21 +71,16 @@ fn main() {
 		
         v.push(Robot { x: temp_x, y: temp_y, heading: temp_heading });
     }
-    println!("{}",v.len());   
-    
-   /*
-    for (index, bot) in &mut v.iter().enumerate() {
-        (*bot).x += 100.;
-        println!("x location of bot is {}, iteration is {}", bot.x, index);
-    }
-   */
-   
+    println!("{}",v.len());      
    	
+ 
     
     for bot in &mut v {
         (*bot).x += 1.;
         println!("x location of bot is {}", bot.x);
     }
+    
+*/    
     let y = rand::random::<f32>();
     println!("{}", y);
     
