6d311c85deaaec2c99cfcf775426e169289afc4c
NealS <poptire@gmail.com>
2015-08-02 20:12:03 -0500

    add framework for generate and predict functions

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@
 /*
 
 //Some Rust references:
-
+------------------------------------------------
 	let x: &[(usize, usize)] = &[(1,2), (3,9)];
 	for &(a,b) in x.iter() {
 		println!("a is {}, b is {}", a, b);
 	}
-	
+------------------------------------------------
+let x: &[(f32, f32)] = &[(1.0,2.0), (3.0,9.0)];
+test_particle.get_distances(x);
+------------------------------------------------	
 */
 
 
@@ -29,9 +32,7 @@ impl Particle {
         self.y += d_y;
         self.heading += d_heading;
     }
-	
 		
-	
     fn get_distances( &mut self, landmarks: &[(f32, f32)] ) {
 		for (i, &(x, y)) in landmarks.iter().enumerate() {    	
 			println!("distance between particle and landmark {} is ({}, {}),", i, self.x - x, self.y - y);		
@@ -39,44 +40,53 @@ impl Particle {
     }
 }
 
-
-
-
-
-fn main() {
-
+fn generate_particles(n: i32) -> Vec<Particle> {
 	
+//    let mut test_particle = Particle { x: 0., y: 0., heading: (3.141/2.) };
+//    test_particle.move_particle(1.5, 1.3, 1.2);
+//    println!("Particle is at ({}, {}), with heading {} radians",
+//               test_particle.x, test_particle.y, test_particle.heading);
 	
-	let StandardNormal(x) = rand::random();
-	println!("gaussian is {}", x);
-	
-    let mut rng = rand::thread_rng();
-    
-    let mut test_particle = Particle { x: 0., y: 0., heading: (3.141/2.) };
-    test_particle.move_particle(1.5, 1.3, 1.2);
-    println!("Particle is at ({}, {}), with heading {} radians",
-               test_particle.x, test_particle.y, test_particle.heading);
-
-	let x: &[(f32, f32)] = &[(1.0,2.0), (3.0,9.0)];
-	test_particle.get_distances(x);
     let mut v: Vec<Particle> = Vec::new();
-    v.push(test_particle);
-    println!("length of vector is {}", v.len());
-    
+//    v.push(test_particle);
 
-    
-    for i in 0 .. 5000 {
+    let mut rng = rand::thread_rng();    
+    for i in 0..n {
 		let mut temp_x: f32 = rng.gen_range(-10.0f32, 4.0e1f32);
 		let mut temp_y: f32 = rng.gen_range(-10.0f32, 4.0e1f32);
 		let mut temp_heading: f32 = rng.gen_range(-10.0f32, 10.0e1f32) % 3.14159;				
 		
         v.push(Particle { x: temp_x, y: temp_y, heading: temp_heading });
     }
-    println!("length of particle vector is {}",v.len());      
-   	
- 
+
+    return v;
+	    	
+}
+
+fn predict(particles: &mut Vec<Particle>) {
+
+    for p in particles {
+        (*p).x += 1.3;
+        println!("x location of particle is {}", p.x);
+    }
+
+}
+
+
+fn main() {
+
+	let StandardNormal(x) = rand::random();
+	println!("gaussian is {}", x);
+	
+    let mut rng = rand::thread_rng();
+
+    let mut v: Vec<Particle> = generate_particles(10);
+	println!("length of particle vector is {}",v.len());      
     let movement_noise: f32 = 0.2;
 	let mut forward_movement = movement_noise * rng.gen_range(-5.0e-1f32, 5.0e-1f32);
+
+	predict(&mut v);
+
 	
     for particle in &mut v {
         (*particle).x += forward_movement + 1.3;
