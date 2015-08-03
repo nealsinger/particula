9713257c0947ff27277184325dbcc237793ce5bc
NealS <poptire@gmail.com>
2015-08-02 22:24:16 -0500

    clean up code, add gaussian sampling

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ extern crate rand;
 use rand::Rng;
 use rand::distributions::normal::StandardNormal;
 use std::mem;
+use std::f32;
 
 struct Particle {
     x: f32,
@@ -63,36 +64,44 @@ fn generate_particles(n: i32) -> Vec<Particle> {
 	    	
 }
 
-fn predict(particles: &mut Vec<Particle>) {
+fn gaussian_sample() -> f32 {
+	let StandardNormal(random_noise) = rand::random();
+	println!("gaussian sample is {}", random_noise);
+	return random_noise as f32;
+}
 
-    for p in particles {
-        (*p).x += 1.3;
-        println!("x location of particle is {}", p.x);
+
+fn predict(particles: &mut Vec<Particle>, mut distance: f32, turning_angle: f32) {
+
+	let distance_noise: f32 = 0.25;
+	let turning_noise: f32 = 0.25;
+
+	distance += distance_noise * gaussian_sample();
+
+	
+	
+	
+	let d_heading: f32 = turning_angle + (turning_noise * gaussian_sample());
+    for p in particles {        
+        (*p).heading = (p.heading + d_heading) % (2.0*f32::consts::PI);
+      	let d_x: f32 = distance * p.heading.cos();	//all this reassignment is intentional,
+		let d_y: f32 = distance * p.heading.sin();	//it helps the compiler infer types.
+        (*p).x += d_x;
+        (*p).y += d_y;        
+        //println!("particle is at ({}, {}), facing {} rads", p.x, p.y, p.heading);
     }
-
 }
 
 
 fn main() {
 
-	let StandardNormal(x) = rand::random();
-	println!("gaussian is {}", x);
-	
-    let mut rng = rand::thread_rng();
-
-    let mut v: Vec<Particle> = generate_particles(10);
+    let mut v: Vec<Particle> = generate_particles(30000);
+        
 	println!("length of particle vector is {}",v.len());      
-    let movement_noise: f32 = 0.2;
-	let mut forward_movement = movement_noise * rng.gen_range(-5.0e-1f32, 5.0e-1f32);
 
-	predict(&mut v);
+	predict(&mut v, 10.1, 1.02);	//array of particles, distance, heading.
 
-	
-    for particle in &mut v {
-        (*particle).x += forward_movement + 1.3;
-        println!("x location of particle is {}", particle.x);
-    }
-    
+
     
     let y = rand::random::<f32>();
     println!("{}", y);
