a95467833ae444a120509e007e080621ffe24c6e
NealS <poptire@gmail.com>
2015-08-09 03:07:39 -0500

    refactored adding probability to particle struct

--- src/main.rs
+++ src/main.rs
@@ -37,11 +37,17 @@ struct Particle {
     x: f32,
     y: f32,
     heading: f32,
+    probability: f32,
 }
 
 
 
 impl Particle {
+
+
+	fn clone(&self) -> Self {
+		return Particle {x: self.x,  y: self.y, heading: self.heading, probability: self.probability}; 
+	}
 
     fn move_particle(&mut self, mut distance: f32, turning_angle: f32) {
 		
@@ -49,13 +55,11 @@ impl Particle {
 	    self.x += (distance * self.heading.cos()) as f32;
 	    self.y += (distance * self.heading.sin()) as f32;
 //	    println!("particle is at ({}, {}), facing {} rads", self.x, self.y, self.heading);
-    
 	}
 
     fn get_distance(&self, lm: &Coordinate) -> f32 {
 			 return ( ((self.x - lm.x).powf(2.0) + (self.y - lm.y).powf(2.0)).sqrt() );	
-    }
-   
+    }   
 }
 
 
@@ -66,17 +70,14 @@ fn predict(particles: &mut Vec<Particle>, mut distance: f32, turning_angle: f32,
 		let distance_with_noise: f32 = distance + noise_values.distance_noise * gaussian_sample();
 		let turning_angle_with_noise: f32 = turning_angle + (noise_values.turning_noise * gaussian_sample());
 
-		p.move_particle(distance_with_noise, turning_angle_with_noise);
-        		
+		p.move_particle(distance_with_noise, turning_angle_with_noise);        		
 	}
-
 }
 
 
 
 
-
-fn take_measurement(particle_at_actual_location: Particle, landmarks: &Vec<Coordinate>) -> Vec<f32> {
+fn take_measurement(particle_at_actual_location: &Particle, landmarks: &Vec<Coordinate>) -> Vec<f32> {
 	
 	let mut sensor_noise: f32 = 0.005;
 	let mut samples: Vec<f32> = Vec::new();
@@ -114,11 +115,11 @@ fn generate_particles(n: i32) -> Vec<Particle> {
 	
     let mut rng = rand::thread_rng();    
     for i in 0..n {
-		let mut temp_x: f32 = rng.gen_range(-10.0f32, 11.0e0f32);
-		let mut temp_y: f32 = rng.gen_range(-10.0f32, 11.0e0f32);
+		let mut temp_x: f32 = rng.gen_range(-10.0f32, 20.0e0f32);
+		let mut temp_y: f32 = rng.gen_range(-10.0f32, 20.0e0f32);
 		let mut temp_heading: f32 = rng.gen_range(-10.0f32, 10.0e1f32) % 3.14159;				
 		
-        v.push(Particle { x: temp_x, y: temp_y, heading: temp_heading});
+        v.push(Particle { x: temp_x, y: temp_y, heading: temp_heading, probability: 1.0});
     }
 
     return v;
@@ -134,66 +135,61 @@ fn gaussian_sample() -> f32 {
 
 
 
-fn update(particles: &mut Vec<Particle>, landmarks: Vec<Coordinate>, measurements: Vec<f32>, noise_values: &Noise) -> Vec<f32> {
+fn update(particles: &mut Vec<Particle>, landmarks: &Vec<Coordinate>, measurements: Vec<f32>, noise_values: &Noise) //-> Vec<f32> 
+{
 
-	let mut particle_weights: Vec<f32> = Vec::new();
+	//let mut particle_weights: Vec<f32> = Vec::new();
 	
 //	println!("particle weights are {} {} {}", particle_weights[0], particle_weights[1], particle_weights[2]);
-	for (p_i,p) in particles.iter().enumerate() {
-		let mut weight: f32 = 1.0;
+	for (p_i,p) in particles.iter_mut().enumerate() {
+		//let mut weight: f32 = 1.0;
 		for (i, lm) in landmarks.iter().enumerate() {
 			//println!("i is {}", i);
 			p.get_distance(lm);
 //			println!("get distance is {}", p.get_distance(lm));
 //			println!("gaussian values are {}, {}, {}", p.get_distance(lm), noise_values.sensor_noise, measurements[i]);
-			weight *= gaussian_distribution(p.get_distance(lm), noise_values.sensor_noise, measurements[i]);
+			p.probability *= gaussian_distribution(p.get_distance(lm), noise_values.sensor_noise, measurements[i]);
 //					println!("weight is {}", weight);
 		}
 //		println!("particle i is {}", p_i);
 //		println!("weight is {}", weight);
-		particle_weights.push(weight);
+		//particle_weights.push(weight);
 	}
-	return particle_weights;
+	//return particle_weights;
 }
 
 
 
 
 
-
-
-fn find_index_of_max(unsorted_vector: &Vec<f32>) -> usize {
+fn find_max_probability(unsorted_particles: &Vec<Particle>) -> f32 {
 //Rust doesn't support Ord for f32, so you can't use .max() or .cmp()
 
 	let mut max_value: f32 = 0.0;
-	let mut index_of_max_value: usize = 0;
 
-	for (i,value) in unsorted_vector.iter().enumerate() {
-		if (value - max_value) > 0.0 {
-			max_value = value.clone();
-			index_of_max_value = i;
+	for part in unsorted_particles.iter() {
+		if (part.probability - max_value) > 0.0 {
+			max_value = part.probability.clone();
 		}	
 	}
-	return index_of_max_value;
+	return max_value;
 }
 
-
-fn resample(particle_probabilities: &Vec<f32>, max_probabilty_index: usize) -> Vec<f32> {
-	let max_probability: f32 = particle_probabilities[max_probabilty_index];
+fn resample(probable_particles: &Vec<Particle> ) -> Vec<Particle> {
+	let max_probability: f32 = find_max_probability(&probable_particles);
 	let mut threshold: f32 = 0.0;
 	let mut rng = thread_rng();
-	let mut n: usize = rng.gen_range(0, particle_probabilities.len() as usize);
-	let mut resampled_probabilities: Vec<f32> = Vec::new();
-	for prob in particle_probabilities {
+	let mut n: usize = rng.gen_range(0, probable_particles.len() as usize);
+	let mut resampled_probabilities: Vec<Particle> = Vec::new();
+	for p in probable_particles.iter() {
 		let temp_rand: f32 = rng.gen();	//TODO: move this to line below using type hinting
 		threshold += temp_rand * 2.0 * max_probability;
 		
-		while (threshold > particle_probabilities[n]) {
-			threshold -= particle_probabilities[n];
-//			println!("n is {}", n);
-			n = (n + 1) % particle_probabilities.len();
+		while (threshold > probable_particles[n].probability) {
+			threshold -= probable_particles[n].probability;
+			n = (n + 1) % probable_particles.len();
 		}
-		resampled_probabilities.push(particle_probabilities[n]);
+		resampled_probabilities.push(probable_particles[n].clone());
 	}
 	return resampled_probabilities;
 }
@@ -211,21 +207,27 @@ Todo:
 
 fn main() {
 
-    let mut v: Vec<Particle> = generate_particles(10000);	
+    let mut v: Vec<Particle> = generate_particles(1000);	
         
 	println!("length of particle vector is {}",v.len());      
 
-	let particle_noise = Noise {distance_noise: 3.0, turning_noise: 1.0, sensor_noise: 1.0};
+	let particle_noise = Noise {distance_noise: 1.0, turning_noise: 1.0, sensor_noise: 1.0};
 	let landmarks: Vec<Coordinate> = generate_landmarks();
 
-    let mut test_particle = Particle { x: 0., y: 0., heading: (0.0) };
-    test_particle.move_particle(10.0, 3.14159/2.0);	
+    let mut test_particle = Particle { x: 0., y: 0., heading: (0.0), probability: 1.0 };
 
-	let mut measurements: Vec<f32> = take_measurement( test_particle, &landmarks);
+	for i in 0..2 {
+	    test_particle.move_particle(10.0, 3.14159/2.0);	
 
-	predict(&mut v, 10.0, 3.14159/2.0, &particle_noise);	//args: array of particles, distance, heading.*
+		let mut measurements: Vec<f32> = take_measurement(&test_particle, &landmarks);
 
-	let mut probabilities: Vec<f32> = update(&mut v, landmarks, measurements, &particle_noise);
+		predict(&mut v, 10.0, 3.14159/2.0, &particle_noise);	//args: array of particles, distance, heading.*
+
+		update(&mut v, &landmarks, measurements, &particle_noise);
+		
+		v = resample(&v);
+	}
+
 /*	
 	for i in 0..probabilities.len() {
 		if  probabilities[i] > 1e-50f32 {	//this can be tuned for more or fewer particles
@@ -235,17 +237,24 @@ fn main() {
 	}
 */
 
-	let max_index: usize = find_index_of_max(&probabilities);
-	println!("max value is {}", max_index);
-	let resampled_probs: Vec<f32> = resample(&probabilities, max_index);
-	for i in 0..100 {
-		println!("resampled prob is {}", resampled_probs[i]);
-	}
+//	let max_index: usize = find_index_of_max(&probabilities);
+//	println!("max value is {}", max_index);
+//	let resampled_probs: Vec<f32> = resample(&probabilities);
+	
+//	for i in 0..100 {
+//		println!("resampled prob is {}", resampled_probs[i]);
+//	}
 	
 	println!("manual gaussian is {}", gaussian_distribution(28.213606, 0.001, 14.135057));
     
+//	let max_prob_index: usize = find_index_of_max(&resampled_probs);
+//	println!("max value is {}", resampled_probs[max_prob_index]);
+//	println!("number of resampled particles is {}", resampled_probs.len());
 
-
+	for p in 0..100 {
+		println!("resampled prob is {}", v[p].probability);
+	}
+	println!("max probability after three runs is {}", find_max_probability(&v));
 }
 
 
