50d18acf3f446ab3665938871fcac242ac07866c
NealS <poptire@gmail.com>
2015-08-10 01:08:02 -0500

    add particle plotting

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,12 @@ use std::mem;
 use std::f32;
 use std::i32;
 
-struct Coordinate {
+extern crate gnuplot;
+use gnuplot::*;
+
+
+
+struct Coordinate_ {  //Added underscore, because otherwise it conflicts with gnuplot.
     x: f32,
     y: f32,
 }
@@ -44,20 +49,19 @@ struct Particle {
 
 impl Particle {
 
-
 	fn clone(&self) -> Self {
 		return Particle {x: self.x,  y: self.y, heading: self.heading, probability: self.probability}; 
 	}
 
     fn move_particle(&mut self, mut distance: f32, turning_angle: f32) {
 		
-	    self.heading = self.heading % (2.0*f32::consts::PI);
+	    self.heading = turning_angle % (2.0*f32::consts::PI);
 	    self.x += (distance * self.heading.cos()) as f32;
 	    self.y += (distance * self.heading.sin()) as f32;
 //	    println!("particle is at ({}, {}), facing {} rads", self.x, self.y, self.heading);
 	}
 
-    fn get_distance(&self, lm: &Coordinate) -> f32 {
+    fn get_distance(&self, lm: &Coordinate_) -> f32 {
 			 return ( ((self.x - lm.x).powf(2.0) + (self.y - lm.y).powf(2.0)).sqrt() );	
     }   
 }
@@ -66,7 +70,6 @@ impl Particle {
 fn predict(particles: &mut Vec<Particle>, mut distance: f32, turning_angle: f32, noise_values: &Noise) {
 
 	for p in particles.iter_mut() {
-
 		let distance_with_noise: f32 = distance + noise_values.distance_noise * gaussian_sample();
 		let turning_angle_with_noise: f32 = turning_angle + (noise_values.turning_noise * gaussian_sample());
 
@@ -77,7 +80,7 @@ fn predict(particles: &mut Vec<Particle>, mut distance: f32, turning_angle: f32,
 
 
 
-fn take_measurement(particle_at_actual_location: &Particle, landmarks: &Vec<Coordinate>) -> Vec<f32> {
+fn take_measurement(particle_at_actual_location: &Particle, landmarks: &Vec<Coordinate_>) -> Vec<f32> {
 	
 	let mut sensor_noise: f32 = 0.005;
 	let mut samples: Vec<f32> = Vec::new();
@@ -92,38 +95,32 @@ fn take_measurement(particle_at_actual_location: &Particle, landmarks: &Vec<Coor
 	return samples;
 }
 
-fn generate_landmarks() -> Vec<Coordinate> {
-	let mut ls: Vec<Coordinate> = Vec::new();
-	ls.push(Coordinate{x: 0.0, y: 0.0});
-	ls.push(Coordinate{x: 0.0, y: 10.0});
-	ls.push(Coordinate{x: 10.0, y: 10.0});
-	ls.push(Coordinate{x: 10.0, y: 0.0});
+fn generate_landmarks() -> Vec<Coordinate_> {
+	let mut ls: Vec<Coordinate_> = Vec::new();
+	ls.push(Coordinate_{x: 0.0, y: 0.0});
+	ls.push(Coordinate_{x: 0.0, y: 10.0});
+	ls.push(Coordinate_{x: 10.0, y: 10.0});
+	ls.push(Coordinate_{x: 10.0, y: 0.0});
 	return ls;
 }
 
 
 
-fn generate_particles(n: i32) -> Vec<Particle> {
-	
-//    let mut test_particle = Particle { x: 0., y: 0., heading: (3.141/2.) };
-//    test_particle.move_particle(1.5, 1.3, 1.2);
-//    println!("Particle is at ({}, {}), with heading {} radians",
-//               test_particle.x, test_particle.y, test_particle.heading);
-
-	
+fn generate_particles(n: u32) -> Vec<Particle> {
+		
     let mut v: Vec<Particle> = Vec::new();
 	
     let mut rng = rand::thread_rng();    
     for i in 0..n {
-		let mut temp_x: f32 = rng.gen_range(-10.0f32, 20.0e0f32);
-		let mut temp_y: f32 = rng.gen_range(-10.0f32, 20.0e0f32);
+		let mut temp_x: f32 = rng.gen_range(-30.0f32, 30.0e0f32);
+		let mut temp_y: f32 = rng.gen_range(-30.0f32, 30.0e0f32);
 		let mut temp_heading: f32 = rng.gen_range(-10.0f32, 10.0e1f32) % 3.14159;				
 		
         v.push(Particle { x: temp_x, y: temp_y, heading: temp_heading, probability: 1.0});
     }
 
     return v;
-	    	
+
 }
 
 fn gaussian_sample() -> f32 {
@@ -135,27 +132,16 @@ fn gaussian_sample() -> f32 {
 
 
 
-fn update(particles: &mut Vec<Particle>, landmarks: &Vec<Coordinate>, measurements: Vec<f32>, noise_values: &Noise) //-> Vec<f32> 
-{
+fn update(particles: &mut Vec<Particle>, landmarks: &Vec<Coordinate_>, measurements: Vec<f32>, noise_values: &Noise) {
 
-	//let mut particle_weights: Vec<f32> = Vec::new();
-	
-//	println!("particle weights are {} {} {}", particle_weights[0], particle_weights[1], particle_weights[2]);
 	for (p_i,p) in particles.iter_mut().enumerate() {
-		//let mut weight: f32 = 1.0;
 		for (i, lm) in landmarks.iter().enumerate() {
-			//println!("i is {}", i);
 			p.get_distance(lm);
 //			println!("get distance is {}", p.get_distance(lm));
 //			println!("gaussian values are {}, {}, {}", p.get_distance(lm), noise_values.sensor_noise, measurements[i]);
 			p.probability *= gaussian_distribution(p.get_distance(lm), noise_values.sensor_noise, measurements[i]);
-//					println!("weight is {}", weight);
 		}
-//		println!("particle i is {}", p_i);
-//		println!("weight is {}", weight);
-		//particle_weights.push(weight);
 	}
-	//return particle_weights;
 }
 
 
@@ -195,72 +181,73 @@ fn resample(probable_particles: &Vec<Particle> ) -> Vec<Particle> {
 }
 
 
-/*
-
-Todo: 
-1. add a probability trait to the particle struct, and modify rest of code accordingly.
-2. make loop that resamples particles, moves the true particle, takes a measurement, and feeds resampled particles into predict, and then into resample.
-3. make the above loop go for like three iterations, and print stats of particle with highest likelihood.
-
-*/
-
-
 fn main() {
-
-    let mut v: Vec<Particle> = generate_particles(1000);	
+	let number_of_particles: usize = 5000;
+    let mut v: Vec<Particle> = generate_particles(number_of_particles as u32);	
         
 	println!("length of particle vector is {}",v.len());      
 
-	let particle_noise = Noise {distance_noise: 1.0, turning_noise: 1.0, sensor_noise: 1.0};
-	let landmarks: Vec<Coordinate> = generate_landmarks();
+	let particle_noise = Noise {distance_noise: 0.50, turning_noise: 0.50, sensor_noise: 1.0};
+	let landmarks: Vec<Coordinate_> = generate_landmarks();
 
-    let mut test_particle = Particle { x: 0., y: 0., heading: (0.0), probability: 1.0 };
+    let mut test_particle = Particle { x: 0.0, y: 0.0, heading: (0.0), probability: 1.0 };
 
-	for i in 0..2 {
-	    test_particle.move_particle(10.0, 3.14159/2.0);	
+	let mut forward_movement: f32 = 10.0;
+	let mut heading_angle: f32 = f32::consts::PI/5.0;
+	let mut rng = rand::thread_rng();    
+	
+	for i in 0..5 {
+	    test_particle.move_particle(forward_movement, heading_angle);
 
 		let mut measurements: Vec<f32> = take_measurement(&test_particle, &landmarks);
 
-		predict(&mut v, 10.0, 3.14159/2.0, &particle_noise);	//args: array of particles, distance, heading.*
-
+		predict(&mut v, forward_movement, heading_angle, &particle_noise);	//args: array of particles, distance, heading.*
 		update(&mut v, &landmarks, measurements, &particle_noise);
-		
 		v = resample(&v);
-	}
+//		plot_positions(&v);		//Generate a frame for each resampling, 
+								//very cool - shows how the samples follow and
+								//cluster around the particle.								
 
-/*	
-	for i in 0..probabilities.len() {
-		if  probabilities[i] > 1e-50f32 {	//this can be tuned for more or fewer particles
-			println!("got one! {}", probabilities[i]);
-			println!("location is ({},{})", v[i].x, v[i].y);
-		}
+		forward_movement = rng.gen_range(0.0f32, 20.0e0f32);
+		heading_angle = rng.gen_range(0.0f32, 3.141e0f32);			
 	}
-*/
-
-//	let max_index: usize = find_index_of_max(&probabilities);
-//	println!("max value is {}", max_index);
-//	let resampled_probs: Vec<f32> = resample(&probabilities);
-	
-//	for i in 0..100 {
-//		println!("resampled prob is {}", resampled_probs[i]);
-//	}
-	
-	println!("manual gaussian is {}", gaussian_distribution(28.213606, 0.001, 14.135057));
     
-//	let max_prob_index: usize = find_index_of_max(&resampled_probs);
-//	println!("max value is {}", resampled_probs[max_prob_index]);
-//	println!("number of resampled particles is {}", resampled_probs.len());
-
-	for p in 0..100 {
+	for p in 0..number_of_particles {
 		println!("resampled prob is {}", v[p].probability);
 	}
-	println!("max probability after three runs is {}", find_max_probability(&v));
+	plot_positions(&v);
+	
+	println!("particle's actual position is ({}, {})", test_particle.x, test_particle.y);	
+	println!("size of particle struct is {}",  mem::size_of::<Particle>());
+	
 }
 
 
 fn gaussian_distribution(mu: f32, sigma: f32, x: f32) -> f32 {
-
 	return f32::consts::E.powf(-1.0*((mu - x).powf(2.0)) / (sigma.powf(2.0)) / 2.0) / (2.0 * f32::consts::PI * (sigma.powf(2.0))).powf(0.5)
-
 }
+
+
+fn plot_positions(v: &Vec<Particle>) {
+
+	let mut x: Vec<f32> = Vec::new();
+	let mut y: Vec<f32> = Vec::new();
+	for p in v.iter() {
+		x.push(p.x);
+		y.push(p.y);
+	}
+
+	let mut fg = Figure::new();
+	fg.axes2d()
+	.points(x.iter(), y.iter(), &[Caption("Particles"), PointSymbol('D'), Color("#ffaa77"), PointSize(1.0)])
+	.set_y_range(Fix(-200.0), Fix(200.0))
+	.set_x_range(Fix(-200.0), Fix(200.0))
+	.set_title("Particle Locations", &[])
+	.set_x_label("X", &[Font("Arial", 12.0), TextColor("red"), Rotate(45.0)])
+	.set_y_label("Y", &[Font("Arial", 12.0), TextColor("red"), Rotate(90.0)]);
+
+	fg.show();
+}
+
+
 
